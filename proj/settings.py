b93fb8e91675e9534cf779a1de2b404199396f45
jovimedina <43058818+jovimedina@users.noreply.github.com>
2024-02-18 12:33:29 -0800

    Update settings.py

--- proj/settings.py
+++ proj/settings.py
@@ -122,11 +122,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_URL = 'static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
+
+#jovi
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    os.path.join(BASE_DIR, 'static'),
+    # Make sure 'staticfiles' is not included here
 ]
+#ends jovi
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=BASE_DIR / 'media'
