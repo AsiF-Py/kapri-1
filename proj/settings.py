39ab743d903b45c43657adfc0ea4816504d0b0db
jovimedina <43058818+jovimedina@users.noreply.github.com>
2024-02-18 12:49:38 -0800

    Update settings.py

--- proj/settings.py
+++ proj/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 #jovi
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR / 'static'),
+    BASE_DIR / 'static')
     # Make sure 'staticfiles' is not included here
 ]
 #ends jovi
